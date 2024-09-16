7c8c8006cea8ca4d81343dd880c7f8f7ece98a7c
Mr.Shahzad.Developer <Mr.Shahzad.Developer@gmail.com>
2024-09-16 13:59:00 +0500

    Initial commit

---
+++ brainWave/src/App.jsx
@@ -0,0 +1,30 @@
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Benifits from './Components/Benifits'
+import Button from './Components/Button'
+import Collabration from './Components/Collabration'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Hero from './Components/Hero'
+import Pricing from './Components/Pricing'
+import Roadmap from './Components/Roadmap'
+import Services from './Components/Services'
+function App() {
+  return (
+    <>
+     
+     <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+      <Header />
+      <Hero />
+      <Benifits />
+      <Collabration />
+      <Services />
+      <Pricing />
+      <Roadmap />
+      <Footer />
+     </div>
+     <ButtonGradient />
+    </>
+  )
+}
+
+export default App
